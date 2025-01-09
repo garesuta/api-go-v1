dc99ba69325ea8abe7dd06298d1b67205d4ee903
garesuta <garesuta@gmail.com>
2025-01-09 21:29:49 +0700

    add cors

--- main.go
+++ main.go
@@ -6,6 +6,9 @@ import (
 	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+
+	// "github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
@@ -40,6 +43,12 @@ func main() {
 
 	// Setup Fiber
 	app := fiber.New()
+	app.Use(cors.New(cors.Config{
+		AllowHeaders:     "Origin,Content-Type,Accept,Content-Length,Accept-Language,Accept-Encoding,Connection,Access-Control-Allow-Origin",
+		AllowOrigins:     "*",
+		AllowCredentials: true,
+		AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
+	}))
 
 	// // CRUD routes
 	app.Get("/jobs", func(c *fiber.Ctx) error {
